fcb63ca0844601b8535c9d55aa0fcdbbfd2fa72f
Jennifer Harden <jkfout1@gmail.com>
2019-03-28 21:10:48 -0500

    completed assignment

---
+++ README.md
@@ -0,0 +1,20 @@
+# Disney Movies Giftastic
+```
+### Link to site: [GifTastic](https://jenharden.github.io/GifTastic/.)
+```
+### Description:
+```
+This is a nice site to explore your love for Disney movies, in gif form.  This site allows you to click on an existing movie button to load 10 random gifs for that movie.  You can also add your own movies and generate gifs for all of the Disney movies you love.  
+
+The gifs will load as "still" images initially, but if you click a gif then you will see the animation of the gif.  If you want to stop the animation, just click the gif again.  
+
+If you would like to know the rating of any gif, hover over the gif, and you will see the rating displayed.
+```
+## Technologies Used:
+```
+HTML 5
+CSS
+Javascript
+JQuery
+API
+```
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,4 +2,101 @@
 
 $(document).ready(function () {
 
+    // Array of movies
+
+    var movies = ["Dumbo", "The Lion King", "The Little Mermaid", "Snow White and the Seven Dwarfs", "Frozen", "Beauty and the Beast", "Cinderella", "Pinocchio", "Bambi"];
+
+    // function for creating the buttons 
+
+    function renderButtons() {
+
+        // Deletes buttons prior to adding new movies, so that there will be no repeat buttons
+
+        $("#buttonsView").empty();
+
+        for (var i = 0; i < movies.length; i++) {
+
+            // Dynamically creating buttons for movies in the array and displaying them in the appropriate div
+
+            var btn = $('<button>')
+            btn.addClass('movie');
+            btn.attr('data-name', movies[i]);
+            btn.text(movies[i]);
+            $("#buttonsView").append(btn);
+            $("#movieInput").val("");
+        }
+    };
+
+    renderButtons();
+
+    // Adds the buttons for movies entered into the input form
+
+    $("#addMovie").on("click", function () {
+        event.preventDefault();
+        var movie = $("#movieInput").val().trim();
+        movies.push(movie);
+        renderButtons();
+        return;
+    });
+
+// Function created to display the gifs
+
+    function displayGifs() {
+        var movie = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + movie + "&api_key=2270exNnb9XWD9fMZnpAS03sgU2MGxpi&limit=10";
+
+        // Makes the AJAX request with the queryURL above
+
+        $.ajax({ url: queryURL, method: "GET" }).done(function (response) {
+           
+            // Saves the results as a variable
+
+            var results = response.data;
+
+            // For loop goes through each gif and adds these variables
+
+            for (var i = 0; i < results.length; i++) {
+
+                // Creates a generic div to hold the results
+
+                var gifDiv = $('<div class=gifs>');
+                var movieGif = $('<img>');
+                
+                movieGif.attr('src', results[i].images.fixed_height_still.url);
+
+                // Displays the movie rating on hover
+
+                movieGif.attr('title', "Rating: " + results[i].rating);
+                movieGif.attr('data-still', results[i].images.fixed_height_still.url);
+                movieGif.attr('data-state', 'still');
+                movieGif.addClass('gif');
+                movieGif.attr('data-animate', results[i].images.fixed_height.url);
+
+                gifDiv.append(movieGif)
+
+
+                $("#requestedGifs").prepend(gifDiv);
+            }
+
+        });
+    }
+
+    // Function to either animate the gifs or stop animation
+
+    $(document).on('click', '.gif', function () {
+        var state = $(this).attr('data-state');
+        if (state == 'still') {
+            $(this).attr('src', $(this).data('animate'));
+            $(this).attr('data-state', 'animate');
+        } else {
+            $(this).attr('src', $(this).data('still'));
+            $(this).attr('data-state', 'still');
+        };
+    });
+
+
+    // Function for displaying movie gifs
+
+    $(document).on("click", ".movie", displayGifs);
+
 });
